23db14626bb10748e9139ff0af5505e0c5243c5c
cyrussnake <qcsrnepomuceno@tip.edu.ph>
2023-12-04 21:35:31 +0800

    Workout History

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,47 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+import { StyleSheet, View, Text, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
 import Header from "../components/Header";
 import CalendarStrip from "react-native-calendar-strip";
 
 const HomeScreen = ({ templates }) => {
-  const [selectedDateRange, setSelectedDateRange] = useState({
-    startDate: new Date(),
-    endDate: new Date(),
-  });
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [workoutDetails, setWorkoutDetails] = useState({});
+  const [workoutPlan, setWorkoutPlan] = useState("");
+  const [exerciseType, setExerciseType] = useState("");
+  const [weight, setWeight] = useState("");
+
+  const onDateSelected = (date) => {
+    setSelectedDate(date);
+  };
 
   const onWeekChanged = (start, end) => {
-    setSelectedDateRange({ startDate: start, endDate: end });
+    // For simplicity, let's use the start date of the week
+    setSelectedDate(start);
   };
+
+  const handleAddWorkout = () => {
+    // Store workout details for the selected date
+    const updatedWorkoutDetails = {
+      ...workoutDetails,
+      [selectedDate.toISOString()]: {
+        workoutPlan,
+        exerciseType,
+        weight,
+      },
+    };
+
+    // Update state with the new workout details
+    setWorkoutDetails(updatedWorkoutDetails);
+
+    // Reset input fields
+    setWorkoutPlan("");
+    setExerciseType("");
+    setWeight("");
+  };
+
+  // Get workout details for the selected date
+  const selectedDateDetails =
+    workoutDetails[selectedDate.toISOString()] || {};
 
   return (
     <SafeAreaView style={styles.container}>
@@ -41,8 +71,54 @@ const HomeScreen = ({ templates }) => {
           dateNameStyle={styles.dateText}
           highlightDateNumberStyle={styles.highlightDateText}
           highlightDateNameStyle={styles.highlightDateText}
+          onDateSelected={onDateSelected}
           onWeekChanged={onWeekChanged}
         />
+
+        {/* Workout Plan Form */}
+        <View style={styles.formContainer}>
+          <Text style={styles.formLabel}>Workout Plan:</Text>
+          <TextInput
+            style={styles.formInput}
+            value={workoutPlan}
+            onChangeText={(text) => setWorkoutPlan(text)}
+          />
+
+          <Text style={styles.formLabel}>Exercise Type:</Text>
+          <TextInput
+            style={styles.formInput}
+            value={exerciseType}
+            onChangeText={(text) => setExerciseType(text)}
+          />
+
+          <Text style={styles.formLabel}>Weight (KG):</Text>
+          <TextInput
+            style={styles.formInput}
+            value={weight}
+            onChangeText={(text) => setWeight(text)}
+            keyboardType="numeric"
+          />
+
+          <TouchableOpacity style={styles.addButton} onPress={handleAddWorkout}>
+            <Text style={styles.addButtonLabel}>Add Workout Plan</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Display workout details for the selected date */}
+        <View style={styles.selectedDateDetailsContainer}>
+          <Text style={styles.selectedDateDetailsLabel}>
+            Selected Date Details:
+          </Text>
+          <Text style={styles.selectedDateDetailsText}>
+            Workout Plan: {selectedDateDetails.workoutPlan || "-"}
+          </Text>
+          <Text style={styles.selectedDateDetailsText}>
+            Exercise Type: {selectedDateDetails.exerciseType || "-"}
+          </Text>
+          <Text style={styles.selectedDateDetailsText}>
+            Weight (KG): {selectedDateDetails.weight || "-"}
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -80,6 +156,46 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginTop: 10,
     marginLeft: 20,
+  },
+  formContainer: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  formLabel: {
+    color: "white",
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  formInput: {
+    backgroundColor: "white",
+    height: 40,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+  },
+  addButton: {
+    backgroundColor: "#CE0E2D",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  addButtonLabel: {
+    color: "white",
+    fontWeight: "bold",
+  },
+    selectedDateDetailsContainer: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  selectedDateDetailsLabel: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  selectedDateDetailsText: {
+    color: "white",
+    fontSize: 16,
+    marginBottom: 5,
   },
 });
 
